dfc4d51dbc30a98e73efbba719e9f58068cea526
Yehor <bolythefender@gmail.com>
2023-05-10 18:20:23 +0400

    chore: fix lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,40 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['react', 'react-native', 'prettier', 'import'],
+  rules: {
+    // import rules
+    'sort-imports': [
+      'error',
+      { ignoreCase: true, ignoreDeclarationSort: true },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['external', 'builtin'],
+          'internal',
+          ['sibling', 'parent'],
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: '@(react|react-native)',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@src/**',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['internal', 'react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
 };
